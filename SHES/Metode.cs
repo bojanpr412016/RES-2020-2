316d5990f9e53d30f24de548ea97e7c912450a95
maka031 <mlazovic1998@gmail.com>
2020-06-14 00:30:24 +0200

    Baterija dodavanje

---
+++ SHES/Metode.cs
@@ -0,0 +1,43 @@
+﻿using Komponente;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SHES
+{
+    public class Metode : IMetode
+    {
+        public void dodavanjeBaterije(string i, int ms, int k)
+        {
+
+                Baterija b = new Baterija(i, ms, k);
+
+                using (var db = new SHESContex())
+                {
+                    db.Baterije.Add(b);
+                    db.SaveChanges();
+                Console.WriteLine("Baterija uspesno dodata!");
+                }
+        }
+
+
+        
+
+        public void dodavanjePanela(string i, int snaga)
+        {
+
+            SolarniPanel sp = new SolarniPanel(i, snaga);
+
+            using (var db = new SHESContex())
+            {
+                db.Paneli.Add(sp);
+                db.SaveChanges();
+                Console.WriteLine("Solarni panel uspesno dodat!");
+
+            }
+
+        }
+    }
+}
--- SHES/Program.cs
+++ SHES/Program.cs
@@ -12,55 +12,51 @@ namespace SHES
     {
         static void Main(string[] args)
         {
-            string izbor = "";
-            Console.WriteLine("Unesite opciju:");
-            Console.WriteLine("1. Dodaj solarni panel");
-            Console.WriteLine("2. Dodaj bateriju");
-            Console.WriteLine("0. Izlaz");
-            izbor = Console.ReadLine();
-
-            if (!System.Text.RegularExpressions.Regex.IsMatch("^[0-9]", izbor))
+            while (true)
             {
-                int x = Int32.Parse(izbor);
-                switch (x)
+                string izbor = "";
+                Console.WriteLine("Unesite opciju:");
+                Console.WriteLine("1. Dodaj solarni panel");
+                Console.WriteLine("2. Dodaj bateriju");
+                Console.WriteLine("0. Izlaz");
+                izbor = Console.ReadLine();
+                Metode m = new Metode();
+                if (!System.Text.RegularExpressions.Regex.IsMatch("^[0-9]", izbor))
                 {
-                    case 1:
-                        Console.WriteLine("Unesite ime panela: ");
-                        string i = Console.ReadLine();
-                        Console.WriteLine("Unesite maksimalnu snagu panela: ");
-                        int snaga = Int32.Parse(Console.ReadLine());
+                    int x = Int32.Parse(izbor);
+                    switch (x)
+                    {
+                        case 1:
+                            Console.WriteLine("Unesite ime panela: ");
+                            string i = Console.ReadLine();
+                            Console.WriteLine("Unesite maksimalnu snagu panela: ");
+                            int snaga = Int32.Parse(Console.ReadLine());
+                            m.dodavanjePanela(i, snaga);
+                            break;
+                        case 2:
+                            Console.WriteLine("Unesite ime baterije:");
+                            string baterijaIme = Console.ReadLine();
+                            Console.WriteLine("Unesite maksimalnu snagu baterije:");
+                            int maxSnaga = Int32.Parse(Console.ReadLine());
+                            Console.WriteLine("Unesite kapacitet baterije:");
+                            int kapacitet = Int32.Parse(Console.ReadLine());
+                            m.dodavanjeBaterije(baterijaIme, maxSnaga, kapacitet);
+                            break;
+                        case 0:
 
-                        dodavanjePanela(i, snaga);
-                        break;
-                    case 2:
-                        Console.WriteLine("Unesite ime baterije: ");
-                        string baterijaIme = Console.ReadLine();
-                        
+                            break;
+                        default:
+                            break;
+                    }
 
-
-                        break;
-                    case 0:
-                        
-                        break;
-                    default:
-                        break;
                 }
-
-            }
-            else
-            {
-                Console.WriteLine("Morate izabrati jednu od postojecih opcija!");
-            }
-            void dodavanjePanela(string i, int snaga)
-            {
-                SolarniPanel sp = new SolarniPanel(i, snaga);
-
-                using (var db = new SHESContex())
+                else
                 {
-                    db.Paneli.Add(sp);
-                    db.SaveChanges();
+                    Console.WriteLine("Morate izabrati jednu od postojecih opcija!");
                 }
             }
+            
+            
 
         }
 
