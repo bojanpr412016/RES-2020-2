39c1271bc1c47e1ca7b5265e3955ad20be0708bc
maka031 <mlazovic1998@gmail.com>
2020-06-13 22:23:20 +0200

    Baterija komponenta

---
+++ Komponente/Baterija.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Komponente
+{
+    public class Baterija
+    {
+        private string ime;
+        private int maksimalnaSnaga;
+        private int kapacitet;
+
+        public string Ime { get => ime; set => ime = value; }
+        public int MaksimalnaSnaga { get => maksimalnaSnaga; set => maksimalnaSnaga = value; }
+        public int Kapacitet { get => kapacitet; set => kapacitet = value; }
+
+        public Baterija()
+        {
+        }
+
+        public Baterija(string ime, int maksimalnaSnaga, int kapacitet)
+        {
+            Ime = ime;
+            MaksimalnaSnaga = maksimalnaSnaga;
+            Kapacitet = kapacitet;
+        }
+
+
+    }
+}
